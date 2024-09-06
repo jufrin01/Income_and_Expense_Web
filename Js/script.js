260df71c65a56ed32fb07e8bdff1930c923d58da
jufrin01 <jufrinaha111@gmail.com>
2024-09-06 17:01:18 +0700

    add code html  element script btn and tbody

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+let BtnIncome = document.getElementById('btn_income');
+let BtnExpense = document.getElementById('btn_expenses');
+
+let tBodyIncomes = document.getElementById('tbody-incomes');
+let tBodyExpenses = document.getElementById('tbody-expenses');
+
